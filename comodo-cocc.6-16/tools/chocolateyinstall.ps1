041fbd8f74e0ed6e4d44bc6f11bfa5e786c77eaa
inventicauk <its-admin@inventica.co.uk>
2018-04-04 11:02:28 +0100

    Comodo One Client - Communication April Update to Version 6.17

---
+++ comodo-cocc.6-16/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://dl.cmdm.comodo.com/download/COCC.msi'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  softwareName  = 'COMODO Client - Communication*'
+  checksum      = '2485D80FA0B64128C58306E5E4AE665EF8825DD6473519C5FEC6B6B68EC07D1B'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ comodo-cocc.6-17/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://dl.cmdm.comodo.com/download/COCC.msi'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  softwareName  = 'COMODO Client - Communication*'
+  checksum      = '8ACB9866EFE10C3BEB83283D91A66C5971E78421A39C6779159E38E2284DB509'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyPackage @packageArgs
